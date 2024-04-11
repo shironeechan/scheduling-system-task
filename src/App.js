7ae7e947c84a5278f84c9eae0c0fcfeb3a778da7
DX INTERN <dxintern@DxFORMs-Mac-mini.local>
2024-04-11 13:35:21 +0800

    delete comment

--- src/App.js
+++ src/App.js
@@ -1,31 +1,3 @@
-// //import logo from './logo.svg';
-// import './App.css';
-// import DataInput from './components/DataInput';
-// import DataTable from './components/DataTable';
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-//         <DataInput/>
-//         <p>
-//           DEPARTURE AND ARRIVAL TABLE
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//         <DataTable/>
-//       </header>
-//     </div>
-//   );
-// }
-
-
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import './App.css';
 import InputForm from './components/InputForm';
